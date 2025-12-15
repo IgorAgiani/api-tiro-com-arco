96887d2137280927003af29d0c85558bd3c6e304
Igor Agiani Silva <igor_011@hotmail.com.br>
2025-12-15 10:20:48 -0300

    Refactor: Retira os dados de simulação da Main e configuração de carga inicial de dados data.sql

--- src/main/java/br/com/tirocomarco/api/ApiApplication.java
+++ src/main/java/br/com/tirocomarco/api/ApiApplication.java
@@ -1,43 +1,12 @@
 package br.com.tirocomarco.api;
 
-import br.com.tirocomarco.api.model.Arqueiro;
-import br.com.tirocomarco.api.repository.ArqueiroRepository;
-import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.Bean;
-
-import java.time.LocalDate;
-import java.util.Arrays;
 
 @SpringBootApplication
 public class ApiApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(ApiApplication.class, args);
-    }
-
-    @Bean // carrega os dados iniciais de teste no banco H2 toda vez que a aplicação iniciar.
-    CommandLineRunner initDatabase(ArqueiroRepository arqueiroRepository) {
-        return args -> {
-
-            arqueiroRepository.deleteAll(); // Limpa o banco antes de inserir para evitar duplicatas a cada reinicialização.
-
-            Arqueiro arq1 = new Arqueiro(
-                    null,
-                    "Igor Agiani Silva",
-                    LocalDate.of(1994, 6, 17),
-                    "Composto Open Paralimpico",
-                    "LA Archery - São Paulo (SP)");
-
-            Arqueiro arq2 = new Arqueiro(
-                    null,
-                    "Rodrigo Theodisio Dewes",
-                    LocalDate.of(1987, 3, 15),
-                    "Composto Open Paralimpico",
-                    "LA Archery - São Paulo (SP)");
-
-            arqueiroRepository.saveAll(Arrays.asList(arq1, arq2));
-        };
     }
 }
---
+++ src/main/resources/data.sql
@@ -0,0 +1,23 @@
+INSERT INTO tb_arqueiros (nome, data_nascimento, categoria, clube)
+VALUES ('Igor Agiani Silva', '1994-06-17', 'Composto Open Masculino Paralimpico', 'LA Archery - São Paulo (SP)');
+
+INSERT INTO tb_arqueiros (nome, data_nascimento, categoria, clube)
+VALUES ('Jane Karla', '1987-03-15', 'Composto Open Feminino Paralimpico', 'LA Archery - São Paulo (SP)');
+
+INSERT INTO tb_arqueiros (nome, data_nascimento, categoria, clube)
+VALUES ('Jean Pargolo', '1992-08-30', 'Composto Masculino', 'Arqueria Bandeirantes - São Paulo (SP)');
+
+INSERT INTO tb_arqueiros (nome, data_nascimento, categoria, clube)
+VALUES ('Larissa Aparecida', '1995-10-20', 'Composto Feminino', 'Arqueria Bandeirantes - São Paulo (SP)');
+
+INSERT INTO tb_arqueiros (nome, data_nascimento, categoria, clube)
+VALUES ('Fabiola Dergovits', '1988-11-11', 'Recurvo Open Feminino Paralimpico', 'Arqueria Bandeirantes - São Paulo (SP)');
+
+INSERT INTO tb_arqueiros (nome, data_nascimento, categoria, clube)
+VALUES ('Heriberto Rocca', '1985-07-22', 'Recurvo Open Masculino Paralimpico', 'Clube Mirá - São Paulo (SP)');
+
+INSERT INTO tb_arqueiros (nome, data_nascimento, categoria, clube)
+VALUES ('Marcos D´Almeida', '1990-05-05', 'Recurvo Masculino', 'Dispara Brasil - Rio de Janeiro (RJ)');
+
+INSERT INTO tb_arqueiros (nome, data_nascimento, categoria, clube)
+VALUES ('Ana Luisa Caetano', '2000-12-25', 'Recurvo Feminino', 'Dispara Brasil - Rio de Janeiro (RJ)');
